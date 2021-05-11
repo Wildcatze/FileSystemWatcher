c939dfd116438ccf246265fbd1f35f4660a3c74c
Wildcatze <alexpebicicleta@gmail.com>
2021-05-11 22:26:39 +0200

    Small improvements

--- Program.cs
+++ Program.cs
@@ -11,34 +11,53 @@ namespace FileSystemWatcher_Demo
     {
         static void Main(string[] args)
         {
-            var fileSystemWatcher = new FileSystemWatcher(@"E:\School\Code Practice\C#\Exam Project\New CSV")
-            {
-                Filter = "*.csv",
+            String WatchingHere = @"E:\School\Code Practice\C#\Exam Project\New CSV";
+            var fileSystemWatcher = new FileSystemWatcher(WatchingHere)
+            { 
+                Filter = "*.csv", 
                 NotifyFilter = NotifyFilters.FileName | NotifyFilters.Size | NotifyFilters.Attributes,
                 EnableRaisingEvents = true
             };
 
-            fileSystemWatcher.Changed += OnActionOccurOnFolderPath;
-            fileSystemWatcher.Created += OnActionOccurOnFolderPath;
-            fileSystemWatcher.Deleted += OnActionOccurOnFolderPath;
-            fileSystemWatcher.Renamed += OnActionOccurOnFolderPath;
+            fileSystemWatcher.Changed += ActionOccurOnFileChange;
+            fileSystemWatcher.Created += ActionOccurOnFileCreated;
+            fileSystemWatcher.Deleted += ActionOccurOnFileDeled;
+            fileSystemWatcher.Renamed += ActionOccurOnFileRenamed;
 
-            Console.WriteLine("Press any key to exit.");
+            Console.WriteLine("File System Watcher is now running." +
+                "\nThe following path is being monitored: '" + WatchingHere + "'." +
+                "\nAny changes made will appear below." +
+                "\nIf you want to terminate," +
+                "\npress any key and ENTER.");
             Console.ReadLine();
         }
 
-        private static void OnActionOccurOnFolderPath(object sender, FileSystemEventArgs e)
+        private static void ActionOccurOnFileCreated(object sender, FileSystemEventArgs e)
         {
-            Console.WriteLine("*** A file change has occured ***");
-            Console.WriteLine(e.ChangeType);
+            Console.WriteLine("*** Hey! A new file was added.");
+            Console.WriteLine(e.ChangeType + ".");
             Console.WriteLine(e.Name);
         }
 
-        private static void OnFileRenameOccur(object sender, RenamedEventArgs e)
+        private static void ActionOccurOnFileChange(object sender, FileSystemEventArgs e)
         {
-            Console.WriteLine("*** Attention. The file was renamed ***");
-            Console.WriteLine($"*** The old file name was: {e.OldName}");
-            Console.WriteLine($"*** The new file name is: {e.Name}");
+            Console.WriteLine("*** A file change has occured.");
+            Console.WriteLine(e.ChangeType + ".");
+            Console.WriteLine(e.Name);
+        }
+
+        private static void ActionOccurOnFileDeled(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("*** The following file has been deleted:");
+            Console.WriteLine(e.ChangeType + ".");
+            Console.WriteLine(e.Name);
+        }
+
+        private static void ActionOccurOnFileRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine("*** The following file has been renamed:");
+            Console.WriteLine($"The old file name was: '{e.OldName}'.");
+            Console.WriteLine($"The new file name is: '{e.Name}'.");
         }
     }
 }
